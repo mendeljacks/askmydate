5a1968ba11039eb7d39e2b9b1c7f40a26da220b5
Mendel <mendeljacks@gmail.com>
2021-05-04 00:19:18 -0400

    Added home page

--- src/main_store.js
+++ src/main_store.js
@@ -5,9 +5,15 @@ class MainStore {
     constructor() {
         makeAutoObservable(this)
     }
+
     sheet = {}
     question_index = 0
-    
+    started = false
+
+    get question_count() {
+        return this.sheet?.feed?.entry?.length - 1
+    }
+
     fetch_questions = async () => {
         const response = await axios.get('https://spreadsheets.google.com/feeds/list/1hqVAmY7fvhTZnmrqhLHps_hXe6fI9wjc1rOJsDEhq3s/od6/public/values?alt=json')
         this.sheet = response.data
@@ -19,6 +25,10 @@ class MainStore {
     
     back = () => {
         this.question_index --
+    }
+
+    begin = () => {
+        this.started = true
     }
 }
 
